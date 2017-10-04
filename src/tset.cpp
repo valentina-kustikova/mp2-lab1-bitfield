2265681ba9407f38fe540f3d63f48f22c21ea455
annapirova <anna.yu.malova@gmail.com>
2017-10-04 15:21:35 +0300

    constructir is corrected

--- src/tset.cpp
+++ src/tset.cpp
@@ -7,17 +7,17 @@
 
 #include "tset.h"
 
-TSet::TSet(int mp) : BitField(-1)
+TSet::TSet(int mp) : BitField(mp)
 {
 }
 
 // конструктор копирования
-TSet::TSet(const TSet &s) : BitField(-1)
+TSet::TSet(const TSet &s) : BitField(mp)
 {
 }
 
 // конструктор преобразования типа
-TSet::TSet(const TBitField &bf) : BitField(-1)
+TSet::TSet(const TBitField &bf) : BitField(mp)
 {
 }
 
